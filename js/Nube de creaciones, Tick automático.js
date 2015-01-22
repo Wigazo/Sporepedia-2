1baab7ce881d429dcf0ba35b4b1a81d558f6b4b5
Wigazo <wigazo@hotmail.com>
2015-01-21 23:04:32 -0300

    Corrección de error hecho sin querer cuando hacía los archivos

--- js/Nube de creaciones, Tick automático.js
+++ js/Nube de creaciones, Tick automático.js
@@ -1,13 +1,8 @@
-//PosiciÛn: En todas las p·ginas
+//Posici√≥n: En todas las p√°ginas
 //Para nubeCreaciones.user.js (y otros posibles usos)
 $(document).ready(function () {
     if (location.href.match(/&tick=true/)) {
         document.forms['post'].post_icon_1.checked=true;
         $('.submit-buttons input[name="post"]').click()
     }
-});
-
-
-
-
 });
