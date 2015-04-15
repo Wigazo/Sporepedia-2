cb1c34c589862e6610fa6576a33777fdbafc124f
Wigazo <wigazo@hotmail.com>
2015-04-15 13:59:52 -0300

    Subidor de imgur: Barra de progreso
    http://www.sporepedia2.com/t17315p48-sporepedia-2-5-soltar-imagenes-en-el-editor-de-texto-para-subirlas#339036

--- js/Subidor Imgur.js
+++ js/Subidor Imgur.js
@@ -1,41 +1,52 @@
 //Posición: En todas las páginas
 $(document).ready(function() { $(document).ready(function() {
     if ($('#text_editor_textarea + * textarea').length) {
+        var dataImgs;
+        var subidasImgs;
         var subirImagenes = function(e) {
+            esconderDropZone();
             e.stopPropagation();
             e.preventDefault();
             if (e.dataTransfer.files) {
-                cantidadImgs = e.dataTransfer.files.length;
+                dataImgs = new Array();
                 for (var i = 0; i < e.dataTransfer.files.length; i++) {
                     if (e.dataTransfer.files[i].type.match("image.*")) {
-                        var reader = new FileReader();
-                        reader.onload = function(evt) {
-                            var data = new FormData();
-                            data.append('image', evt.target.result.replace(/.*,/, ''));
-                            var xhr = new XMLHttpRequest();
-                            xhr.open('POST', 'https://api.imgur.com/3/upload', true);
-                            xhr.setRequestHeader('Authorization', 'Client-ID 2800bab5227ab24');
-                            xhr.onreadystatechange = function() {
-                                if (xhr.readyState == 4 && xhr.status == 200) {
-                                    unaMenos();
-                                    $('#text_editor_textarea').sceditor("instance").insertText("[img]" + JSON.parse(xhr.responseText).data.link + "[/img]");
-                                }
-                            };
-                            xhr.send(data);
-                        };
-                        reader.readAsDataURL(e.dataTransfer.files[i]);
-                    } else {
-                        unaMenos();
+                        dataImgs.push(e.dataTransfer.files[i]);
                     }
                 }
-            } else {
-                esconderDropZone();
+                if (dataImgs.length) {
+                    subidasImgs = 0;
+                    dropZone.before('<div id="imgurPB" style="width: 200px; left: 50%; margin-left: -100px; top: 60%; position: absolute; background-color: #1F1F1F; border-radius: 10px;"></div>');
+                    $("#imgurPB").append('<div id="imgurPB-hecho" style="position: absolute; background-color: #85BF25; height: 100%; width: 0%; border-radius: 10px; z-index: -1;"></div>');
+                    $("#imgurPB").append('<div id="imgurPB-label" style="font-size: 25px; color: white; text-align:center;">...</div>');
+                    $("#imgurPB-label").text(subidasImgs + "/" + dataImgs.length);
+                    $("#imgurPB").append('<img src="http://i.imgur.com/LJ7Nuqs.gif" style="position: absolute;left: 6px; top: 1px;">');
+                }
+                for (var i = 0; i < dataImgs.length; i++) {
+                    var reader = new FileReader();
+                    reader.onload = function(evt) {
+                        var data = new FormData();
+                        data.append('image', evt.target.result.replace(/.*,/, ''));
+                        var xhr = new XMLHttpRequest();
+                        xhr.open('POST', 'https://api.imgur.com/3/upload', true);
+                        xhr.setRequestHeader('Authorization', 'Client-ID 2800bab5227ab24');
+                        xhr.onreadystatechange = function() {
+                            if (xhr.readyState == 4 && xhr.status == 200) {
+                                $('#text_editor_textarea').sceditor("instance").insertText("[img]" + JSON.parse(xhr.responseText).data.link + "[/img]");
+                                subidasImgs++;
+                                if (subidasImgs < dataImgs.length) {
+                                    $("#imgurPB-hecho").css("width", +subidasImgs / dataImgs.length * 100 + "%");
+                                    $("#imgurPB-label").text(subidasImgs + "/" + dataImgs.length);
+                                } else {
+                                    $("#imgurPB").remove();
+                                }
+                            }
+                        };
+                        xhr.send(data);
+                    };
+                    reader.readAsDataURL(dataImgs[i]);
+                }
             }
-        };
-        var cantidadImgs = 0;
-        var unaMenos = function() {
-            cantidadImgs--;
-            if (cantidadImgs < 1) esconderDropZone();
         };
         var resaltarDropZone = function(e) {
             e.stopPropagation();
