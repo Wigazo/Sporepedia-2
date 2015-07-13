8d3eb6a99032674065003d2a3aedcd069fdb6369
Wigazo <wigazo@hotmail.com>
2015-07-13 09:15:24 -0300

    NdC: Alt+B hacía saltar cartel de "Prevenir pérdidas de mensajes"
    http://www.sporepedia2.com/t13436p560-proyecto-nube-de-creaciones-fase-de-recuperacion#350998

--- js/Nube de creaciones, Tick automático.js
+++ js/Nube de creaciones, Tick automático.js
@@ -1,8 +1,8 @@
 //Posición: En todas las páginas
 //Para nubeCreaciones.user.js (y otros posibles usos)
-$(document).ready(function () {
+$(document).ready(function () { $(document).ready(function () {
     if (location.href.match(/&tick=true/)) {
         document.forms['post'].post_icon_1.checked=true;
         $('.submit-buttons input[name="post"]').click()
     }
-});
+})});
