b771729e927eff113fffa187467bea46233e4c79
Wigazo <wigazo@hotmail.com>
2015-12-19 14:56:34 -0300

    Banners aleatorios con tooltip
    http://www.sporepedia2.com/t18754p16-banners-de-imgur#368622

--- js/Banner aleatorio.js
+++ js/Banner aleatorio.js
@@ -1,160 +1,73 @@
-//Posición: En todas las páginas
-$(function(){var banner = new Array();
+﻿//Posición: En todas las páginas
+var bannerAleatorio = function() {
+    if (localStorage.getItem("banners") !== null && new Date().getTime() - JSON.parse(localStorage.getItem("banners")).ultimaActualizacion < 1000 * 60 * 60 * 24 * 15) {
+        var banners = JSON.parse(localStorage.getItem("banners")).lista;
+        var banner = banners[Math.floor(Math.random() * banners.length)];
+        $('#logo img').attr("src", banner.link);
+        if (/^u\d+$/.test(banner.description)) {
+            //Banner usuario
+            $.get(banner.description, function(data) {
+                tooltipBanner("<a href='" + this.url + "'>" + data.split("<title>Perfil - ")[1].split("</title>")[0] + "</a>");
+            });
+        } else {
+            //Banner creación concurso
+            tooltipBanner(banner.description);
+        }
+    } else {
+        //Descargar lista banners si no está guardada o tiene más de 15 días
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://api.imgur.com/3/album/J29OG/images');
+        xhr.setRequestHeader('Authorization', 'Client-ID 2800bab5227ab24');
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                if (xhr.status == 200) {
+                    var imgAlbum = JSON.parse(xhr.responseText).data;
+                    var banners = {
+                        "ultimaActualizacion": new Date().getTime(),
+                        "lista": []
+                    };
+                    for (var i = 0; i < imgAlbum.length; i++) {
+                        banners.lista.push({
+                            link: imgAlbum[i].link,
+                            description: imgAlbum[i].description
+                        });
+                    }
+                    localStorage.setItem("banners", JSON.stringify(banners));
+                    bannerAleatorio();
+                }
+            }
+        };
+        xhr.send();
+    }
+};
 
-banner[0]=['http://i.imgur.com/zEBUQsu.png', "Esta imagen tiene tooltip"];
-banner[1]=['http://i.imgur.com/OyWzzct.png'];
-banner[2]=['http://i.imgur.com/4ElDoXr.png'];
-banner[3]=['http://i.imgur.com/DGKEH6E.png'];
-banner[4]=['http://i.imgur.com/0uL7OD3.png'];
-banner[5]=['http://i.imgur.com/pvFItU0.png'];
-banner[6]=['http://i.imgur.com/Vn24jqx.png'];
-banner[7]=['http://i.imgur.com/VYVtCQ0.png'];
-banner[8]=['http://i.imgur.com/21Wg80w.png'];
-banner[9]=['http://i.imgur.com/X27bZ3B.png'];
-banner[10]=['http://i.imgur.com/XsDQrHy.png'];
-banner[11]=['http://i.imgur.com/rPmdlEd.png'];
-banner[12]=['http://i.imgur.com/hpt4uGI.png'];
-banner[13]=['http://i.imgur.com/FmS2M8H.png'];
-banner[14]=['http://i.imgur.com/rXKUmFh.png'];
-banner[15]=['http://i.imgur.com/mObSE4D.png'];
-banner[16]=['http://i.imgur.com/PWl2J4X.png'];
-banner[17]=['http://i.imgur.com/eazDNtF.png'];
-banner[18]=['http://i.imgur.com/bce1HRt.png'];
-banner[19]=['http://i.imgur.com/CKbvoaK.png'];
-banner[20]=['http://i.imgur.com/2S5zgfk.png'];
-banner[21]=['http://i.imgur.com/8QpfI2z.png'];
-banner[22]=['http://i.imgur.com/eEgYMXf.png'];
-banner[23]=['http://i.imgur.com/0uL7OD3.png'];
-banner[24]=['http://i.imgur.com/uy3vmaT.png'];
-banner[25]=['http://i.imgur.com/IkgSlYm.png'];
-banner[26]=['http://i.imgur.com/xtiOFkl.png'];
-banner[27]=['http://i.imgur.com/VpQZhfM.png'];
-banner[28]=['http://i.imgur.com/XG9OnQQ.png'];
-banner[29]=['http://i.imgur.com/ISE7WFK.png'];
-banner[30]=['http://i.imgur.com/nklpxt4.png'];
-banner[31]=['http://i.imgur.com/c5TCikU.png'];
-banner[32]=['http://i.imgur.com/5tx0Gg7.png'];
-banner[33]=['http://i.imgur.com/6CzQSEI.png'];
-banner[34]=['http://i.imgur.com/ndSPP0z.png'];
-banner[35]=['http://i.imgur.com/6dEqaHu.png'];
-banner[36]=['http://i.imgur.com/i7xSqd7.png'];
-banner[37]=['http://i.imgur.com/ZDA0iWN.png'];
-banner[38]=['http://i.imgur.com/yqW4hsD.png'];
-banner[39]=['http://i.imgur.com/RSDpSO7.png'];
-banner[40]=['http://i.imgur.com/V04v93j.png'];
-banner[41]=['http://i.imgur.com/Ioz6OQq.png'];
-banner[42]=['http://i.imgur.com/ACdlI9W.png'];
-banner[43]=['http://i.imgur.com/x09ogyU.png'];
-banner[44]=['http://i.imgur.com/cVJDMXe.png'];
-banner[45]=['http://i.imgur.com/nl6Q19a.png'];
-banner[46]=['http://i.imgur.com/L6sCzZZ.png'];
-banner[47]=['http://i.imgur.com/68Sg7J1.png'];
-banner[48]=['http://i.imgur.com/zhCECz5.png'];
-banner[49]=['http://i.imgur.com/tgNQJab.png'];
-banner[50]=['http://i.imgur.com/RjsMMqw.png'];
-banner[51]=['http://i.imgur.com/5yPIhGc.png'];
-banner[52]=['http://i.imgur.com/9rNCD78.png'];
-banner[53]=['http://i.imgur.com/isJHZOg.png'];
-banner[54]=['http://i.imgur.com/iAT7tvA.png'];
-banner[55]=['http://i.imgur.com/fDErU2A.png'];
-banner[56]=['http://i.imgur.com/fYsjDXi.png'];
-banner[57]=['http://i.imgur.com/5JyWFvG.png'];
-banner[58]=['http://i.imgur.com/QHdmywH.png'];
-banner[59]=['http://i.imgur.com/6F3JvnJ.png'];
-banner[60]=['http://i.imgur.com/WpzQTSz.png'];
-banner[61]=['http://i.imgur.com/ujl5BLW.png'];
-banner[62]=['http://i.imgur.com/Ad6dpEM.png'];
-banner[63]=['http://i.imgur.com/DdZ8Z1L.png'];
-banner[64]=['http://i.imgur.com/CwKxUAo.png'];
-banner[65]=['http://i.imgur.com/f2Ie5R8.png'];
-banner[66]=['http://i.imgur.com/BmYqNaW.png'];
-banner[67]=['http://i.imgur.com/9rGHmmr.png'];
-banner[68]=['http://i.imgur.com/QNvTjbn.png'];
-banner[69]=['http://i.imgur.com/HV2SJWR.png'];
-banner[70]=['http://i.imgur.com/jsnDwmj.png'];
-banner[71]=['http://i.imgur.com/gAFkpps.png'];
-banner[72]=['http://i.imgur.com/YAH9hqV.png'];
-banner[73]=['http://i.imgur.com/eZSyUIB.png'];
-banner[74]=['http://i.imgur.com/zNP1EjH.png'];
-banner[75]=['http://i.imgur.com/SVXGfw7.png'];
-banner[76]=['http://i.imgur.com/RM9L3QR.png'];
-banner[77]=['http://i.imgur.com/axTOWHA.png'];
-banner[78]=['http://i.imgur.com/wbmI68e.png'];
-banner[79]=['http://i.imgur.com/2QSvyXC.png'];
-banner[80]=['http://i.imgur.com/RFMPAbp.png'];
-banner[81]=['http://i.imgur.com/E7z8PB8.png'];
-banner[82]=['http://i.imgur.com/NFEk7a6.png'];
-banner[83]=['http://i.imgur.com/DMDmt9w.png'];
-banner[84]=['http://i.imgur.com/aV5Mtpa.png'];
-banner[85]=['http://i.imgur.com/qgkXAIA.png'];
-banner[86]=['http://i.imgur.com/R7YtowB.png'];
-banner[87]=['http://i.imgur.com/Nuiv7CG.png'];
-banner[88]=['http://i.imgur.com/L3Jtv55.png'];
-banner[89]=['http://i.imgur.com/W1aKHQw.png'];
-banner[90]=['http://i.imgur.com/NuRdFmd.png'];
-banner[91]=['http://i.imgur.com/4DarZAe.png'];
-banner[92]=['http://i.imgur.com/Smmsztp.png'];
-banner[93]=['http://i.imgur.com/JfgsV2x.png'];
-banner[94]=['http://i.imgur.com/9x46vBn.png'];
-banner[95]=['http://i.imgur.com/zfhvdIQ.png'];
-banner[96]=['http://i.imgur.com/7JnWPvt.png'];
-banner[97]=['http://i.imgur.com/8YxSCAt.png'];
-banner[98]=['http://i.imgur.com/YH24jQ4.png'];
-banner[99]=['http://i.imgur.com/0SjNbjS.png'];
-banner[100]=['http://i.imgur.com/zEBUQsu.png'];
-banner[101]=['http://i.imgur.com/zEBUQsu.png'];
-banner[102]=['http://i.imgur.com/zEBUQsu.png'];
-banner[103]=['http://i.imgur.com/zEBUQsu.png'];
-banner[104]=['http://i.imgur.com/zEBUQsu.png'];
-banner[105]=['http://i.imgur.com/zEBUQsu.png'];
-banner[106]=['http://i.imgur.com/zEBUQsu.png'];
-banner[107]=['http://i.imgur.com/zEBUQsu.png'];
-banner[108]=['http://i.imgur.com/zEBUQsu.png'];
-banner[109]=['http://i.imgur.com/zEBUQsu.png'];
-banner[110]=['http://i.imgur.com/bmVR9BZ.png'];
-banner[111]=['http://i.imgur.com/GIn8k4z.png'];
-banner[112]=['http://i.imgur.com/eCNS3H8.png'];
-banner[113]=['http://i.imgur.com/NoYTKw2.png'];
-banner[114]=['http://i.imgur.com/bgTlGWf.png'];
-banner[115]=['http://i.imgur.com/vhXBYb1.png'];
-banner[116]=['http://i.imgur.com/x1zZeKU.png'];
-banner[117]=['http://i.imgur.com/V15nvF9.png'];
-banner[118]=['http://i.imgur.com/5LQ8jAf.png'];
-banner[119]=['http://i.imgur.com/hUmfrdN.png'];
-banner[120]=['http://i.imgur.com/8nqxzOv.png'];
-banner[121]=['http://i.imgur.com/LVWsZVm.png'];
-banner[122]=['http://i.imgur.com/8A6nMwT.png'];
-banner[123]=['http://i.imgur.com/IU8BYUH.png'];
-banner[124]=['http://i.imgur.com/82pJtRq.png'];
-banner[125]=['http://i.imgur.com/FhOJaWT.png'];
-banner[126]=['http://i.imgur.com/6YutJ7w.png'];
-banner[127]=['http://i.imgur.com/yvM8miY.png'];
-banner[128]=['http://i.imgur.com/v80nG0U.png'];
-banner[129]=['http://i.imgur.com/VTiKAVA.png'];
-banner[130]=['http://i.imgur.com/SglqEBU.png'];
-banner[131]=['http://i.imgur.com/NuoILzn.png'];
-banner[132]=['http://i.imgur.com/NIlK2Ki.png'];
-banner[133]=['http://i.imgur.com/dN4BTaH.png'];
-banner[134]=['http://i.imgur.com/XZah6ZV.png'];
-banner[135]=['http://i.imgur.com/UyN3U71.png'];
-banner[136]=['http://i.imgur.com/Of971wH.png'];
-banner[137]=['http://i.imgur.com/4xy3HMJ.png'];
-banner[138]=['http://i.imgur.com/coknbDG.png'];
-banner[139]=['http://i.imgur.com/YhnNWOt.png'];
-banner[140]=['http://i.imgur.com/ZK4NEVu.png'];
-banner[141]=['http://i.imgur.com/DNNya6y.png'];
-banner[142]=['http://i.imgur.com/aXakXUa.png'];
-banner[143]=['http://i.imgur.com/nXE1pz0.png'];
-banner[144]=['http://i.imgur.com/NkHrojf.png'];
-banner[145]=['http://i.imgur.com/I4xdYXV.png'];
-banner[146]=['http://i.imgur.com/rZDavPf.png'];
-banner[147]=['http://i.imgur.com/KOQKkvM.png'];
-banner[148]=['http://www.mediafire.com/convkey/3296/riu5n86d433jatazg.jpg?size_id=6'];
+var tooltipBanner = function(tooltip) {
+    if (tooltip !== null) {
+        $('#logo img').attr("title", "Sporepedia2");
+        $("#logo img").tooltip({
+            content: tooltip,
+            position: {
+                my: "left bottom",
+                at: "left bottom"
+            },
+            show: 1000,
+            close: function(event, ui) {
+                ui.tooltip.hover(
+                    function() {
+                        $(this).stop(true).fadeTo(500, 1);
+                    },
+                    function() {
+                        $(this).remove();
+                    }
+                );
+            }
+        });
+    }
+};
 
-var selectedBanner = Math.floor(Math.random()*banner.length);
-if (banner[selectedBanner.length] > 1) {
-	$('#logo img').attr("src",banner[selectedBanner][0]).attr("title",banner[selectedBanner][1]);
-} else {
-	$('#logo img').attr("src",banner[selectedBanner][0]);
-}
+
+$(function() {
+    if (window.localStorage) {
+        bannerAleatorio();
+    }
 });
